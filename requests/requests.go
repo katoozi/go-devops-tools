f893f490a7da0cd8aadaa249ffa845b355fda2c7
mohammad katoozi <k2527806@gmail.com>
2019-06-23 22:09:31 +0430

    create post request, default client request, request with query string

--- requests/requests.go
+++ requests/requests.go
@@ -5,12 +5,51 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func get() {
 	resp, err := http.Get("https://httpbin.org/get")
+	// resp, err := http.Get("https://httpbin.org/get") // you can send querystring
 	if err != nil {
-		log.Fatalln("There is a Error during the Get Request.")
+		log.Fatalln("Unable to get response")
+	}
+	defer resp.Body.Close()
+
+	content, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatalln("Unable to read from response body.")
+	}
+	fmt.Println(string(content))
+}
+
+func post() {
+	resp, err := http.Post("https://httpbin.org/post?search=something", "text/plain",
+		strings.NewReader("this the request content"))
+	if err != nil {
+		log.Fatalln("Unable to get response")
+	}
+	defer resp.Body.Close()
+
+	content, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatalln("Unable to read from response body.")
+	}
+	fmt.Println(string(content))
+}
+
+func customeClient() {
+	client := http.DefaultClient
+	req, err := http.NewRequest("GET", "https://httpbin.org/get", nil)
+	if err != nil {
+		log.Fatalln("Unable to create request.")
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln("Unable to get response")
 	}
 	defer resp.Body.Close()
 
