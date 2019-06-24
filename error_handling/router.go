6ea121af69cca877d2ef539c448d88a08980f1de
mohammad katoozi <k2527806@gmail.com>
2019-06-24 21:28:33 +0430

    write comments for error handling

--- error_handling/router.go
+++ error_handling/router.go
@@ -5,13 +5,16 @@ import (
 	"net/http"
 )
 
+// HandlerFuc is a simple function that handle the response
 type HandlerFuc func(*http.Response)
 
+// ResponseRouter is a struct that store the response handlers
 type ResponseRouter struct {
 	Handlers       map[int]HandlerFuc
 	DefaultHanderl HandlerFuc
 }
 
+// NewRouter is a factory function that create new ResponseRouter object
 func NewRouter() *ResponseRouter {
 	return &ResponseRouter{
 		Handlers: make(map[int]HandlerFuc),
@@ -21,10 +24,12 @@ func NewRouter() *ResponseRouter {
 	}
 }
 
+// Register will save the response handler
 func (r *ResponseRouter) Register(status int, handler HandlerFuc) {
 	r.Handlers[status] = handler
 }
 
+// Process will detect the response status code and choose the handler function
 func (r *ResponseRouter) Process(resp *http.Response) {
 	f, ok := r.Handlers[resp.StatusCode]
 	if !ok {
